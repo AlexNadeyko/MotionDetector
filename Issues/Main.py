02c781b630827c3d74682ba218c481cf9d7b46ec
KeterSCP <38229504+KeterSCP@users.noreply.github.com>
2020-02-28 22:06:29 +0100

    Add files via upload

---
+++ Issues/Main.py
@@ -0,0 +1,29 @@
+from MotionDetection import MotionDetector, DetectorHandler
+from FaceAnalysis import FaceAnalyzer
+from time import sleep
+import cv2
+
+detector = MotionDetector()
+# Here we start out detector
+detector.start()
+
+# Simulating some work
+for i in range(20):
+    print(i)
+    sleep(0.2)
+
+# Here we stop detector
+detector.stop()
+
+# Simulate some work after stopping the detector
+for i in range(20):
+    print(i)
+    sleep(0.2)
+
+
+# Start detector again
+detector.start()
+
+# And here the problem is
+# Camera window does not pop up
+# Fixes: ???
