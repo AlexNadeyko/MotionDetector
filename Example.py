6cd7311b1fa36822978007dc8d2f240afc59fc12
KeterSCP <38229504+KeterSCP@users.noreply.github.com>
2020-02-29 12:14:50 +0100

    Added example for test run.

---
+++ Example.py
@@ -0,0 +1,10 @@
+from MotionDetection import MotionDetector, DetectorHandler
+from FaceAnalysis import FaceAnalyzer
+
+detector = MotionDetector()
+analyzer = FaceAnalyzer()
+
+handler = DetectorHandler(detector, analyzer)
+
+detector.start()
+handler.listen()
