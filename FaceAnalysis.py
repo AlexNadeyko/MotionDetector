86c8883ea8fe849371ef151732894ad27e37fc4d
KeterSCP <twinker3228@gmail.com>
2020-04-05 15:44:30 +0200

    Changed face location model to haardetector. Fixed bugs in code

--- FaceAnalysis.py
+++ FaceAnalysis.py
@@ -1,21 +1,24 @@
 import cv2
 import numpy as np
-import random
 import face_recognition
+import datetime
+from threading import Thread
 import Data.DataProcessing as dp
 
-class FaceAnalyzer:
-    def __init__(self, tolerance=0.6, model='hog'):
+class FaceAnalyzer(Thread):
+    def __init__(self, tolerance=0.6, model='haar'):
         self.model = model
         self.tolerance = tolerance
-        self.__known_faces_dir = r'./Faces/Known'
+        self.__face_cascade = cv2.CascadeClassifier(r'./Cascades/haarcascade_frontalface_default.xml')
         self.__unknown_faces_dir = r'./Faces/Unknown'
         self.__encodings = dp.load_encodings()
+        self.current_face = None
+        self.current_encoding = None
 
 
     def process_motion(self, motion):
         found_faces, faces = self.find_faces(motion)
-        if found_faces is True:
+        if found_faces:
             unknown_faces = []
             for face in faces:
                 face_encoding = face_recognition.face_encodings(face)
@@ -29,11 +32,14 @@ class FaceAnalyzer:
                 results = face_recognition.compare_faces(known_encodings_list, face_encoding, self.tolerance)
                 is_known = np.any(results)
 
-                if is_known is False:
+                if not is_known:
                     unknown_faces.append(face)
                 else:
-                    known_name = list(self.__encodings.keys())[np.where(results)[0][0]]
-                    print(f'Face known [{known_name}] (Remove this message after release)')
+                    try:
+                        known_name = list(self.__encodings.keys())[np.where(results)[0][0]] # Here sometimes exception occurs
+                        print(f'Face known [{known_name}] (Remove this message after release)')
+                    except:
+                        pass
 
             if len(unknown_faces) > 0:
                 return unknown_faces
@@ -41,25 +47,49 @@ class FaceAnalyzer:
                 return None
 
 
-
-
     def find_faces(self, motion):
         img = motion.crop_box(5)
 
-        # Check if motion frame is big enough to analyze
-        if img.shape[0] * img.shape[1] < 800 * 10:
-            return (False, None)
+        def rotate_image(image, angle):
+            image_center = tuple(np.array(image.shape[1::-1]) / 2)
+            rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+            result = None
+            try:
+                result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+            except:
+                pass
+            return result
 
-        face_boxes = face_recognition.face_locations(img, model=self.model)
+        # Due to the fact that haardetector is not rotation invariant, we try to rotate images on 360 degrees to find possibly rotated faces
+        for angle in range(0, 360, 20):
+            rotated = rotate_image(img, angle)
 
-        if len(face_boxes) == 0:
-            return (False, None)
+            # Check if frame is big enough to analyze
+            if rotated is None or rotated.shape[0] * rotated.shape[1] < 800 * 10:
+                continue
 
-        faces = [img[top:bottom, left:right] for (top, right, bottom, left) in face_boxes]
-        return (True, faces)
+            faces = []
+
+            if self.model == 'haar':
+                face_boxes = self.__face_cascade.detectMultiScale(rotated, 1.25, minNeighbors=4)
+
+                if len(face_boxes) == 0:
+                    continue
+                faces = [rotated[y:y + h, x:x + w] for (x, y, w, h) in face_boxes]
+
+            elif self.model == 'hog':
+                face_boxes = face_recognition.face_locations(img, model=self.model)
+
+                if len(face_boxes) == 0:
+                    continue
+                faces = [img[top:bottom, left:right] for (top, right, bottom, left) in face_boxes]
+
+            return (True, faces)
+        return (False, None)
 
 
     def save_unknown_faces(self, faces):
         print('Saving unknown faces')
         for f in faces:
-            cv2.imwrite(rf'./Faces/Unknown/{random.randint(1, 100000)}.jpg', f)
+            filename = rf'{self.__unknown_faces_dir}/{str(datetime.datetime.now())[11:-7].replace(":", ".")}.jpg'
+            cv2.imwrite(filename, f)
