283677480ac110a0ab5e3c12e630119c5e8d6e73
KeterSCP <twinker3228@gmail.com>
2020-04-10 17:06:21 +0200

    Found a solution to fix this issue

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
-from multiprocessing import Queue
+from multiprocessing import Queue, Process
 from threading import Thread
 import face_recognition
 import cv2
 import time
 
-frames = Queue(10)
+frames = Queue(1)
 cam = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalface_default.xml')
 
-def capture():
+
+def capture(frame_queue):
     prevTime = 0
     while True:
         ret, frame = cam.read()
@@ -18,17 +19,18 @@ def capture():
         fps = 1 / (sec)
         str = "FPS : %0.1f" % fps
         cv2.putText(frame, str, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), thickness=2)
-        if not frames.full():
-            frames.put(frame)
+        if not frame_queue.full():
+            frame_queue.put(frame)
         cv2.imshow('Capture', frame)
         cv2.waitKey(1)
+        # cv2.imwrite(r'C:/Users/twink/Desktop/temp/1.jpg', frame)
 
 
-def process(case):
+def process(case, frame_queue):
     prevTime = 0
     while True:
-        if not frames.empty():
-            frame = frames.get()
+        if not frame_queue.empty():
+            frame = frame_queue.get().copy()
             curTime = time.time()
             sec = curTime - prevTime
             prevTime = curTime
@@ -94,7 +96,6 @@ def process(case):
             elif case == 'FaceLandmarks':
                 face_landmarks_list = face_recognition.face_landmarks(frame)
                 for face_landmarks in face_landmarks_list:
-
                     for facial_feature in face_landmarks.keys():
                         for point in face_landmarks[facial_feature]:
                             cv2.circle(frame, (point[0], point[1]), 2, (0, 255, 0))
@@ -112,7 +113,7 @@ case1 = 'OpencvProcessing'
 case2 = 'FaceLandmarks'
 case3 = 'HaarFaceDetection'
 
-# Insert case1 or case2 to th2 args and see te difference in FPS
+# Insert case1, case2 or case3 to th2 args and see te difference in FPS
 
 # Capture video form shows it's FPS on top left corner
 # Processed video form has it's FPS on bottom (do not mind the top one, it's just the copy from initial frame)
@@ -120,8 +121,15 @@ case3 = 'HaarFaceDetection'
 # So we can see that opencv multithread processing does not influence the performance of an original video,
 # but method from face_recognition library in some way slows down initial video thread, what is quite strange
 
+### UPD 04.10.2020 ###
+# Found a solution. It seems like we must use Processes instead of Threads
+# I suppose the issue was such as the face_recognition method was too performance demanding,
+# so that if we use only one process but two threads, method took all the resources from the process
+# and the original video thread had like 20-30% of the process resources.
+# If we use two processes, they will not share common threads, so the original video stream will run smoothly
+
 if __name__ == '__main__':
-    th1 = Thread(target=capture)
-    th2 = Thread(target=process, args=(case3,))
+    th1 = Process(target=capture, args=(frames,))
+    pr1 = Process(target=process, args=(case3,frames,))
     th1.start()
-    th2.start()
+    pr1.start()
