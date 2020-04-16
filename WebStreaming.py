3b149725068cd570c7c2c9f47bb1e61c1a14394e
Alex Nadeyko <nadeykoa@gmail.com>
2020-04-16 19:00:02 +0200

    Fixed size of displaying image in streaming. Renamed file from index.html to main_page.html.

--- WebStreaming.py
+++ WebStreaming.py
@@ -9,60 +9,40 @@ import cv2
 detector = MotionDetector()
 analyzer = FaceAnalyzer()
 
-
-
-# create instance of web application
 app = Flask(__name__)
 
-# set acting on main page
+
 @app.route('/')
 def welcome_page():
     return render_template('welcome_page.html')
 
-# @app.route('/')
-# def index():
-#     return render_template('welcome_page.html')
 
 @app.route('/main_page/')
 def main_page():
-    return render_template('index.html')
+    return render_template('main_page.html')
 
-# @app.route('/welcome_page/')
-# def welcome_page():
-#     return render_template('welcome_page.html')
 
-# function that prepare current frame to sending
 def generate():
-    # loop over frames from the output stream
     while True:
-
-
         if detector.frames.empty():
             continue
 
         current_frame = detector.frames.get()
-        # encode the frame in JPEG format
         (flag, encodedImage) = cv2.imencode(".jpg", current_frame)
 
-        # ensure the frame was successfully encoded
         if not flag:
-
             continue
 
-        # yield the output frame in the byte format
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
                bytearray(encodedImage) + b'\r\n')
 
-# streaming frames
+
 @app.route("/video_feed")
 def video_feed():
-    # return the response generated along with the specific media
-    # type (mime type)
     return Response(generate(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-# check if this is the main thread of execution
 if __name__ == "__main__":
     detector.start()
     analyzer.start()
